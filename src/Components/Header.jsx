0b23d9d25caa80158e1dc742b1e3482d4317e9d4
Nintoryan <thehappystudio4@gmail.com>
2021-04-14 16:00:37 +0300

    First draft, devided to big components

---
+++ src/Components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <header className="header">
+            <img src="https://cdn.logo.com/hotlink-ok/logo-social-sq.png" alt="logo"/>
+        </header>
+        );
+}
+export default Header;
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Profile = () => {
+    return(
+        <div className="content">
+            <div>
+                <img src="https://static.vecteezy.com/system/resources/previews/001/419/144/non_2x/geometric-3d-cubes-pattern-free-vector.png"></img>
+            </div>
+            <div>
+                ava + desctiption
+            </div>
+            <div>
+                my post
+                <div>
+                    New post
+                </div>
+                <div>
+                    Post 1
+                </div>
+                <div>
+                    Post 2
+                </div>
+            </div>
+        </div>
+        );
+}
+export default Profile;
